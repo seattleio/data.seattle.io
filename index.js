289b59db9330c2ceb059352c7884ab86cbb57c46
sethvincent <sethvincent@gmail.com>
2015-04-02 16:18:11 -0700

    v1.0.0

---
+++ index.js
@@ -0,0 +1,29 @@
+var response = require('response');
+
+var server = require('flatsheet')({
+  site: {
+    title: 'data.seattle.io',
+    email: 'hi@seattle.io',
+    url: 'http://data.seattle.io', // include port here if not at port 80
+    contact: 'Seth Vincent'
+  },
+  db: __dirname + '/data'
+});
+
+server.route('/', function (req, res) {
+  if (!res.account) {
+    return response()
+      .html(server.render('index', {
+        account: { username: 'friend' }
+      }))
+      .pipe(res);
+  }
+  else {
+    res.writeHead(302, { 'Location': '/sheets' });
+    res.end();
+  }
+});
+
+server.listen((process.env.PORT || 3333), function () {
+  console.log('server started at 127.0.0.1:' + (process.env.PORT || 3333));
+});
